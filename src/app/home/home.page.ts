5cd8b7a2daa3fc93ff4ffeacc4b64bb20ca83a53
michael ganesan <cookieganesan@gmail.com>
2018-10-05 00:46:01 -0400

    Added code for the service and the home page!

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,10 +1,52 @@
-import { Component } from '@angular/core';
+import { Note } from './../interfaces/note';
+import { Component, OnInit } from '@angular/core';
+import { AlertController, NavController } from '@ionic/angular'
+import { NotesService } from '../services/notes.service';
+
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit {
+  title: string = "Notes";
+
+  constructor(public notesService:NotesService, private alertCtrl: AlertController, private navCtrl: NavController) {
+
+  }
+
+  ngOnInit(): void {
+     this.notesService.load();
+
+  }
+
+  addNote(){
+        this.alertCtrl.create({
+           header: 'New Note',
+           message: 'What should the title of the note be?',
+           inputs:[
+            {
+              type: 'text',
+              name: 'title'
+            }
+           ],
+           buttons:[
+             {
+               text: 'Cancel'
+             },
+             {
+               text: 'Save',
+               handler: (data:Note)=>{
+                 this.notesService.createNote(data.title);
+               }
+             }
+           ]
+
+        }).then((alert)=>{
+            alert.present();
+        });
+
+  }
 
 }
---
+++ src/app/services/notes.service.ts
@@ -0,0 +1,70 @@
+import { Injectable } from '@angular/core';
+import { Note } from '../interfaces/note';
+import { resolve } from 'url';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class NotesService {
+
+  public notes: Note[] = [];
+  public loaded: boolean = false;
+
+
+  constructor(private storage: Storage) {
+
+  }
+
+  load(): Promise<boolean> {
+
+    return new Promise((resolve) => {
+
+      //get the notes stored in storage
+      this.storage.get("notes").then((notes) => {
+
+        if (notes != null) {
+          this.notes = notes;
+        }
+
+        this.loaded = true;
+        resolve(true);
+
+      });
+    });
+
+  }
+
+  //Save the current array of notes
+  save(): void {
+    this.storage.set('notes', this.notes);
+
+  }
+
+  getNote(id: string): Note {
+
+    //Return the note with the id
+    return this.notes.find(note => note.id === id);
+  }
+
+  createNote(title): void {
+    let id = Math.max(...this.notes.map(note => parseInt(note.id)), 0) + 1;
+
+    this.notes.push(<Note>{
+      id: id.toString(),
+      title: title,
+      content: ''
+    });
+
+    this.save(); //save the note to the storage object
+  }
+
+
+  deleteNote(note): void {
+      let index = this.notes.indexOf(note);
+
+      if(index > -1){
+        this.notes.splice(index,1);
+        this.save();
+      }
+  }
+}
